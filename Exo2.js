e6910e5ecd7814e08528e8a57703bd7386817afb
kossi973 <150068989+kossi973@users.noreply.github.com>
2024-01-25 20:12:47 +0100

    Corrected

--- Exo2.js
+++ Exo2.js
@@ -1,5 +1,5 @@
-let chiffre1 = prompt("Entrez chiffre 1");
-let chiffre2 = prompt("Entrez chiffre 2");
+let chiffre1 = parseInt(prompt("Entrez chiffre 1"));
+let chiffre2 = parseInt(prompt("Entrez chiffre 2"));
 
 console.log(chiffre1 + chiffre2);
 console.log(chiffre1 - chiffre2);
